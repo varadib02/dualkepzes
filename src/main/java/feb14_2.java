0c6d562db9437e762169d4a6ae03014fc7de07c6
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-02-14 14:19:28 +0100

    feb14-2

--- src/main/java/feb14_2.java
+++ src/main/java/feb14_2.java
@@ -2,32 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-import java.util.Scanner;
+
 /**
  *
  * @author User
  */
-public class feb14 {
+public class feb14_2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner bekeres = new Scanner(System.in);
-        int ev=bekeres.nextInt();
-        if ((ev%100!=0 && ev%4==0) || ev%400==0)
-        {
-        System.out.println("Szokoev a "+ ev);
+        
+        for (int szam=1;szam<1000;szam++){
+        if(szam%2==0){
+            if (szam%3==0){
+                System.out.println("Osztaható hárommal");
+            break;
+            }
+            System.out.println("Páros szám");
         }
         else{
-        System.out.println("Nem szokoev a "+ev);
+            System.out.println("Páratlan szám");
         }
-        //ternery 
-        
-        
-        
-        
+        }
     }
     
 }
