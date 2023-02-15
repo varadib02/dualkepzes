4f70c629235d3f005d096804c9fca75dfb33d685
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-02-15 17:36:53 +0100

    2-15-17-36

--- src/main/java/feb15.java
+++ src/main/java/feb15.java
@@ -15,7 +15,6 @@ public class feb15 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         //Generáld le a fibonacci számsor első 12 elemét ciklusok és változók segítségével
         ArrayList<Integer>list=new ArrayList<Integer>();
         int a=1;
@@ -41,31 +40,33 @@ public class feb15 {
         int hossz=match.nextInt();
         int gepW=0;
         int teW=0;
-        int dont=0;
+        int draw=0;
         for (int i = 0; i < hossz; i++) {
             Random r = new Random();
             int gep=r.nextInt(1,4);
             
             System.out.println((i+1)+". Kör a Tipped (1-3): ");
-            Scanner bekeres = new Scanner(System.in);
-            for (int j = 3; j > 0; j--) 
-            {
-                if((bekeres.nextInt()==j && gep==j-1) || (bekeres.nextInt()==j && gep==j+2))
-                {
+            Scanner bekert = new Scanner(System.in);
+            int bekeres=bekert.nextInt();
+            if((bekeres==1 && gep==3)||(bekeres==2 && gep==1)||(bekeres==3 && gep==2)){
+                System.out.println("Nyertél");
                 teW++;
-                }
-                if((gep==j && bekeres.nextInt()==j-1) || (gep==j && bekeres.nextInt()==j+2))
-                {
+            }
+            else if((bekeres==3 && gep==1)||(bekeres==1 && gep==2)||(bekeres==2 && gep==3)){
+                System.out.println("A gép nyert");
                 gepW++;
-                }
-                else{
-                  dont++;  
-                }
+            }
+            else{
+                System.out.println("Döntetlen");
+                draw++;
             }
             
-            bekeres.close();
+            bekert.close();
         }
-        System.out.println("Te nyertél: "+teW+", Gép nyert: "+gepW+", Döntetlen: "+dont);
+        System.out.println("Te nyertél: "+teW+", Gép nyert: "+gepW+", Döntetlen: "+draw);
+        if(teW>gepW) System.out.println("A játék győztese tehát te vagy.");
+        if(teW<gepW) System.out.println("A játék győztese tehát a gép.");
+        else System.out.println("A játéknak nincs győztese.");
         //Extra funkció: a játék elején kérdezd meg a usert hány kört szeretne játszani, 
         //és a játék végén az alábbi módon írd ki a végeredményt: 
         //Te nyertél: 2, Gép nyert: 1, Döntetlen: 3 , A játék győztese tehát te vagy/ a gép.
